bedb6ec75ea6e7f5962a4871c47d357c76797240
Ronald Shutter <r.a.shutter.com>
2021-11-02 07:06:47 -0700

    updated header.js-1

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,19 @@ export const Header = () => {
             </h1>
             <p className="lead hide-on-small">
 
-               **** Full STACK Developer ****
+               A Full STACK Developer with SKILLS in:
+               <li> HTML </li>
+               <li> CSS </li>
+               <li> JAVASCRIPT </li>
+               <li> MYSQL DB</li>
+               <li> MONGO DB </li>
+               <li> EXPRESS JS </li>
+               <li> ... </li>
+               <li> ...</li>
+               <li> ... </li>
+               <li> ... </li>
+             <p> ![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+</p>
             </p>
             <p> </p>
             <p className="lead hide-on-small">
