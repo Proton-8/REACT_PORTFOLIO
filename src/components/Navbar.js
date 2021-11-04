df9b47203117d670f576baea10a18770c39ff2af
Proton-8 <r.a.shutter@gmail.com>
2021-11-04 16:40:17 -0700

    header and nav bar update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,10 +10,14 @@ export const Navbar = () => {
                         Me</a>
                     </li>
                     <li>
-                        <a href="/#"> Projects</a>
+                        <a href="/#"> Portfolio</a>
                     </li>
                     <li>
                         <a href="/#"> Contact Me</a>
+                    </li>
+                    {/* <li> </li> */}
+                    <li>
+                        <a href="https://docs.google.com/document/d/1xV13u2ruQU41HeSb9MlHEL8p5AtqM4gR/edit?usp=sharing&ouid=115475312727942649219&rtpof=true&sd=true">  My Resume</a>
                     </li>
                 </ul>
 <a href="/#" id="openup"> Ronald Shutter</a>
