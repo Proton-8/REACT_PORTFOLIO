9363fb83d6deb6b01352fea30027d7bf78f9c48b
Proton-8 <r.a.shutter@gmail.com>
2022-01-03 21:43:27 -0800

    2022-5

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ export const Footer = () => {
       <p>
         <a className="github" href="https://github.com/Proton-8">My GitHub Acct</a>
 
-        <a href="https://twitter.com">My Twitter Acct</a></p>
+     
       <p>
         <a className="linkedIn" href="https://www.linkedin.com/in/ron-shutter-95613211/">My LinkedIn</a><a href="https://docs.google.com/document/d/1xV13u2ruQU41HeSb9MlHEL8p5AtqM4gR/edit?usp=sharing&ouid=115475312727942649219&rtpof=true&sd=true"> My Resume</a>
       </p>
