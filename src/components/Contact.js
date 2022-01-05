77c694361e836c8038d3da793749bfc06c3b5d08
Ronald Shutter <r.a.shutter.com>
2022-01-05 12:53:48 -0800

    new email test-1

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,93 +1,30 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
-import React, { useState } from 'react';
+export const ContactUs = () => {
+  const form = useRef();
 
-// Import a helper function that will check if the email is valid
-import { validateEmail } from '../utils/helpers';
-
-export const Contact = () => {
-
-
-  // Create state variables for the fields in the form
-  // We are also setting their initial values to an empty string
-  const [email, setEmail] = useState('');
-  const [userName, setUserName] = useState('');
-  const [message, setMessage] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-
-  const handleInputChange = (e) => {
-    // Getting the value and name of the input which triggered the change
-    const { target } = e;
-    const inputType = target.name;
-    const inputValue = target.value;
-
-    // Based on the input type, we set the state of either email, username, and password
-    if (inputType === 'email') {
-      setEmail(inputValue);
-    } else if (inputType === 'userName') {
-      setUserName(inputValue);
-    } else {
-      setMessage(inputValue);
-    }
-  };
-
-  const handleFormSubmit = (e) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
+  const sendEmail = (e) => {
     e.preventDefault();
 
-    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-    if (!validateEmail(email) || !userName) {
-      setErrorMessage('Email or name is invalid');
-      // We want to exit out of this code block if something is wrong so that the user can correct it
-      return;
-
-    }
-    // If everything is good, send an Alert!
-    alert(`Thank you ${userName} for contacting me`);
-
-    // If everything goes according to plan, we want to clear out the input after a successful registration.
-    setUserName('');
-    setMessage('');
-    setEmail('');
-    setErrorMessage('');
-
+    emailjs.sendForm('gmail', 'template_ci6f73c', form.current, 'user_mKp2h1I3qs8RuKuoeS7TP')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
   };
 
   return (
-    <div className="contactStart" id="contact">
-      <p> </p>
-      <p> Contact me using the form below</p>
-      <form className="form">
-        <input
-          value={userName}
-          name="userName"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="Name"
-        />
-        <input
-          value={email}
-          name="email"
-          onChange={handleInputChange}
-          type="email"
-          placeholder="Email"
-        />
-
-        <input
-          value={message}
-          name="message"
-          onChange={handleInputChange}
-          type="message"
-          placeholder="Message"
-        />
-        <button type="button" className="button" onClick={handleFormSubmit}>Submit</button>
-      </form>
-      {errorMessage && (
-        <div>
-          <p className="error-text">{errorMessage}</p>
-        </div>
-      )}
-    </div>
-  )
-
-}
-
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
