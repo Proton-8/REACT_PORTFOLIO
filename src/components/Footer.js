7c02654b00cafe7d647a87c45eb47ff7d61c3459
Proton-8 <r.a.shutter@gmail.com>
2022-01-25 16:44:03 -0800

    Resume pt3

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,7 +16,7 @@ export const Footer = () => {
 
      </p>
       <p>
-        <a className="linkedIn" href="https://www.linkedin.com/in/ron-shutter-95613211/">My LinkedIn</a><a href="https://docs.google.com/document/d/1xV13u2ruQU41HeSb9MlHEL8p5AtqM4gR/edit?usp=sharing&ouid=115475312727942649219&rtpof=true&sd=true"> My Resume</a>
+        <a className="linkedIn" href="https://www.linkedin.com/in/ron-shutter-95613211/">My LinkedIn</a><a href= "https://docs.google.com/document/d/1MfuMqFuQyG7wiIdfwsF5YPtcSD7_MPf1/edit?usp=sharing&ouid=115475312727942649219&rtpof=true&sd=true" > My Resume</a>
       </p>
 
       <h2>Thank you for visiting my site!</h2>
