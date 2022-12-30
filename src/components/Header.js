aed32dac0233335900b0156838a89e4a85caf81f
Ronald Shutter <r.a.shutter@gmail.com>
2022-12-30 14:43:50 -0800

    Updated 12-12-22

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,27 +7,26 @@ export const Header = () => {
                 Ronald Shutter <img src={avatar} alt="User avatar" className="avatar" />
             </h1>
 
-
             <span className="lead hide-on-small">
                 <h3> ABOUT ME:</h3>
                 Having over 20 years in the Engineering field, I am now exploring a new passion.  That is becoming a Full STACK Developer!
-                <p> My new SKILLS include:</p>
-                <li> HTML </li>
-                <li> CSS </li>
+                <p> My new SKILLS include:
+                   <li> CSS </li>
+                   <li> HTML </li>
+                   <li> NODE JS</li>             
                 <li> JAVASCRIPT </li>
                 <li> MYSQL DB</li>
                 <li> MONGO DB </li>
                 <li> EXPRESS JS </li>
-                <li> more to come soon ... </li>
-                <li> ...</li>
-                <li> ... </li>
-                <li> ... </li>
-
-            </span>
-            <p> </p>
-
-
-
+                <li> DOCKER </li>
+                <li> REACT JS</li>
+          
+          I also now have some GAME Developer SKILLS in:
+          <li> UNITY game Design - C#</li>
+          <li> CoCos Game Design - TypeScript</li>
+          <li> PHASER Game Design</li>
+           </p> </span>
+           
 
         </main>
     )
