f07d4fccb82d759e97c51d44012e785059cb46fe
Ronald Shutter <r.a.shutter@gmail.com>
2023-01-03 21:50:27 -0800

    2023-5

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import './App.css';
-import React, { Fragment } from 'react';
+import "./App.css";
+import { Link } from "react-router-dom";
+import React, { Fragment } from "react";
 import { Navbar } from "./components/Navbar";
-import { Header} from "./components/Header";
-import { Project} from "./components/Project";
-import { Contact} from "./components/Contact";
-import { Footer} from "./components/Footer";
+import { Header } from "./components/Header";
+import { Project } from "./components/Project";
+import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
 
 function App() {
-
   return (
     <Fragment>
-      <div id="showcase">   
-   <Navbar/>
-   <Header/>
-    </div>
-    <Project />
-    <br/>
-    <Contact />
-    <Footer />
-       </Fragment>
+      <div id="showcase">
+        <Link to="/about">About</Link>
+        <Navbar />
+        <Header />
+      </div>
+      <Project />
+      <br />
+      <Contact />
+      <Footer />
+    </Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,20 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { HashRouter, Routes, Route } from "react-router-dom";
+import About from "./About";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
-            
       <Routes>
-                
         <Route path="/" element={<App />} />
-                
-        {/* <Route path="/about" element={<About />} /> */}
-              
+        <Route path="/about" element={<About />} />
       </Routes>
-          
     </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
